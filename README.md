64866b04370aa333514182fd4113101681b9c2e0
John Learn <jflearn@logicbrush.com>
2022-11-02 12:28:16 -0400

    readme; tweak command line.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# metrics
+# Metrics
+
+A package of utilities for dealing with coverage methods.
+
+## Installation
+
+```shell
+composer require logicbrush/metrics
+```
+
+## Command Line Functions
+
+A number of metrics-related functions are accessible via the command line.
+
+### Annotate
+
+Use to annotate source code with metric information.  Use with your CI process
+to keep your metrics in front of you as you develop.
+
+```shell
+php ./vendor/bin/metrics annotate «path to coverage.xml» «path to source file»
+```
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -10,7 +10,9 @@ try {
     if (version_compare(PHP_VERSION, '8.0.0', '<')) {
         throw new \ErrorException('PHP Version is lower than 8.0.0. Please upgrade your runtime.');
     }
-    if ( isset($argv[1]) && ($clover = $argv[1]) && isset($argv[2]) && ($file = $argv[2]) ) {
+    if ( isset($argv[1]) && ($argv[1] == 'annotate') &&
+         isset($argv[2]) && ($clover = $argv[2]) && 
+         isset($argv[3]) && ($file = $argv[3]) ) {
         return Logicbrush\Metrics\Bootstrap::createAnnotator( $clover, $file );
     }
     throw new \ErrorException('You must include path to coverage file and file to be annotated as arg 1 and arg 2 respectively.');
